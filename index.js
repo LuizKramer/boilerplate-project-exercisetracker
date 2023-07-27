4aa48d044cb4b165bf7bbfed9c0f7ef915be928e
LuizKramer <luizeduardockramer@gmail.com>
2023-07-27 11:07:12 -0300

    Add a bit functions

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 const express = require('express')
+const User = require('../boilerplate-project-exercisetracker/user')
+const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.post('/api/users', async (req, res) => {
+  const data = req.body;
+  if (!data.username)
+    return res.json({
+      message: "Empty username"
+    });
+  let user = await User.createNewUser(data);
+  return res.json(
+    {
+      username: user.username,
+      _id: user._id
+    }
+  ) 
+})
 
 
 
+app.post('/api/users/:id/exercises', async (req, res) => {
+  console.log(req.params);
+  updatedUser = await User.updateUsers(req);
+  if(!updatedUser){
+    return res.json({
+      message: "Deu Ruim"
+    })
+  }
+  return res.json({
+    _id: updatedUser._id,
+    username: updatedUser.username,
+    date: updatedUser.date,
+    duration: updatedUser.duration,
+    description: updatedUser.description
+  });
+})
+
+app.get('/api/users', async (req, res) =>{ 
+  let allUsers = await User.getAllUsers();
+  return res.json(allUsers);
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
---
+++ user.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  description: String,
+  duration: Number,
+  date: String,
+})
+
+const user = mongoose.model("User", userSchema);
+
+exports.createNewUser = (data , done) => {
+  console.log(data.username)
+  let newUser = new user({ username: data.username });
+  try {
+    newUser.save().then( saveDoc => {
+      saveDoc === newUser;
+    });
+    return newUser;
+  } catch (err) {
+    return err;
+  }
+}
+
+exports.updateUsers = (req) => {
+  console.log(req.body);
+  let date;
+  if (req.body.date) {
+    date = new Date(req.body.date).toDateString();
+  } else {
+    date = new Date().toDateString();
+  }
+
+  console.log(date);
+
+  let query = user.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+      description: req.body.description,
+      duration: req.body.duration,
+      date: date
+    }
+  );
+  return query;
+};
+
+exports.getAllUsers = () => {
+  const query = user.find({})
+  query instanceof mongoose.Query;
+  return query
+}
